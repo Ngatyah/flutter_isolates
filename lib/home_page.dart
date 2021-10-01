4c8b85f030fad75788db10b983406c0cc8bdf5ea
Ngatyah <davidngatyah@gmail.com>
2021-10-01 19:03:38 +0300

    added isolates

---
+++ lib/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isOn = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: const Text('Hello'),
+        ),
+        Center(
+          child: Switch(
+            value: isOn,
+            onChanged: (value) {
+              setState(() {
+                isOn = value;
+                if (isOn) {
+                  print("Happy Day!!");
+                }
+              });
+            },
+          ),
+        ),
+      ],
+    ));
+  }
+}
