ca6f7ca491d762781788f2483b96126dbc28621f
Ngatyah <davidngatyah@gmail.com>
2021-10-04 09:11:13 +0300

    addded permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:workmanager/workmanager.dart';
+import 'dart:async';
+import 'package:telephony/telephony.dart';
 
-import 'home_page.dart';
-
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    print("Native called background task: $backgroundTask"); //simpleTask will be emitted here.
-    return Future.value(true);
-  });
+onBackgroundMessage(SmsMessage message) {
+  debugPrint("onBackgroundMessage called");
 }
 
 void main() {
-  Workmanager().initialize(
-    callbackDispatcher, // The top level function, aka callbackDispatcher
-    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-  );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _message = "";
+  final telephony = Telephony.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  onMessage(SmsMessage message) async {
+    setState(() {
+      _message = message.body ?? "Error reading message body.";
+    });
+  }
+
+  onSendStatus(SendStatus status) {
+    setState(() {
+      _message = status == SendStatus.SENT ? "sent" : "delivered";
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+
+    final bool? result = await telephony.requestPhoneAndSmsPermissions;
+
+    if (result != null && result) {
+      telephony.listenIncomingSms(
+          onNewMessage: onMessage, onBackgroundMessage: onBackgroundMessage);
+    }
+
+    if (!mounted) return;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
       ),
-      home: const HomePage(),
-    );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text("Latest received SMS: $_message")),
+          TextButton(
+              onPressed: () async {
+                await telephony.openDialer("123413453");
+              },
+              child: Text('Open Dialer'))
+        ],
+      ),
+    ));
   }
 }
